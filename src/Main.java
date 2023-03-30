9a02c2227fde894337fef3bd4ea241b55cfe6c3a
Aikusya <butterflyy1211@gmail.com>
2023-03-31 00:44:34 +0600

    Reversing an array

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        int power = scanner.nextInt();
-        int result = power(num, power);
-        System.out.println(result);
+    public static void main (String[] args) {
+        int[] arr = {1, 4, 2, 6};
+        reverseArray(arr, 0, arr.length - 1);
+        printArray(arr, arr.length);
     }
 
-    public static int power(int a, int n){
-        if(n == 0){
-            return 1;
-        }
-        if(n == 1){
-            return a;
-        }
-        return a * power(a, n - 1);
+    static void reverseArray(int[] arr, int start, int end) {
+        int temp;
+        if (start >= end)
+            return;
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArray(arr, start+1, end-1);
+    }
+
+    static void printArray(int[] arr, int size) {
+        for (int i=0; i < size; i++)
+            System.out.print(arr[i] + " ");
     }
 }

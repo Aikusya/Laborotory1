1733b625a7f3d91521ce424c44d239693e883f21
Aikusya <butterflyy1211@gmail.com>
2023-03-31 01:50:06 +0600

    Finding GCD of two numbers

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        int result = binomCoeff(n, k);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int result = gcd(a, b);
         System.out.println(result);
     }
 
-    static int binomCoeff(int n, int k) {
-        if(k == 0){
-            return 1;
-        }
-        if(k == n){
-            return 1;
-        }
-        return binomCoeff(n - 1, k - 1) + binomCoeff(n - 1, k);
-    }
+    public static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
 
+        return gcd(b % a, a);
+    }
 }

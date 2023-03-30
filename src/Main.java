6bfa045054e556f8e0c7ffa33c1dfe2e0bac7a34
Aikusya <butterflyy1211@gmail.com>
2023-03-31 00:10:37 +0600

    Finding a factorial

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,14 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        isPrime(num);
+        int result = factorial(num);
+        System.out.println(result);
     }
 
-    public static void isPrime(int n){
-        int count = 0;
-        for(int i = 1; i <= n / 2; i++){
-            if(n % i == 0){
-                count++;
-            }
+    public static int factorial(int n){
+        if(n == 1){
+            return 1;
         }
-        if(count == 1){
-            System.out.println("is prime");
-        }
-        else{
-            System.out.println("is not prime");
-        }
+        return n * factorial(n - 1);
     }
 }

1f67593d1eb5a6dfe819c95ccb4017b09332d28d
Aikusya <butterflyy1211@gmail.com>
2023-03-31 01:43:34 +0600

    Finding binomial coefficients

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String string = scanner.next();
-        int n = string.length() - 1;
-        check(string, n);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int result = binomCoeff(n, k);
+        System.out.println(result);
     }
 
-    static void check(String string, int n) {
-        if(n == 0){
-            if(Character.isDigit(string.charAt(n))){
-                System.out.println("YES");
-                return;
-            }
-            else{
-                System.out.println("NO");
-                return;
-            }
+    static int binomCoeff(int n, int k) {
+        if(k == 0){
+            return 1;
         }
-        if(Character.isDigit(string.charAt(n))){
-            check(string, n - 1);
+        if(k == n){
+            return 1;
         }
-        else{
-            System.out.println("NO");
-            return;
-        }
+        return binomCoeff(n - 1, k - 1) + binomCoeff(n - 1, k);
     }
 
 }

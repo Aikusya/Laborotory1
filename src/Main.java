cc370ecf3749ab6ebe13d1d5262eb7e9db024529
Aikusya <butterflyy1211@gmail.com>
2023-03-31 00:14:03 +0600

    Finding a fibonacci

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //0 1 1 2 3 5 8 13 ...
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        int result = factorial(num);
+        int result = fibonacci(num);
         System.out.println(result);
     }
 
-    public static int factorial(int n){
+    public static int fibonacci(int n){
+        if(n == 0){
+            return 0;
+        }
         if(n == 1){
             return 1;
         }
-        return n * factorial(n - 1);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }

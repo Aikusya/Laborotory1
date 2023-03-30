6d36426ab72c6b041b468f8b9bd001498ef9a270
Aikusya <butterflyy1211@gmail.com>
2023-03-30 23:11:36 +0600

    Average number of array

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = {10, 1, 32, 3, 45};
-        int min = findMin(arr, arr.length);
-        System.out.println("Min number is " + min);
+        int[] arr = {3, 2, 4, 1};
+        int i = 0;
+        double average = findAvg(arr, i, arr.length);
+        System.out.println("Average number is " + average);
     }
 
-    public static int findMin(int[] arr, int n){
-        if(n == 1){
-            return arr[0];
+    public static double findAvg(int[] arr, int i, int n){
+        if(i == n - 1){
+            return arr[i];
         }
-        else{
-            int min = findMin(arr, n - 1);
-            return Math.min(min, arr[n-1]);
+        if(i == 0){
+            i = i + 1;
+            return ((arr[i - 1] + findAvg(arr, i, n))/n);
         }
+        return (arr[i] + findAvg(arr, i + 1, n));
     }
 }

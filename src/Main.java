4d4bdeb1c89cc9ef58825d6d27b3db9d0bbafae5
Aikusya <butterflyy1211@gmail.com>
2023-03-31 09:10:03 +0600

    Average number of array

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int result = gcd(a, b);
-        System.out.println(result);
+        int[] arr = {3, 2, 4, 1};
+        double average = (findAvg(arr, arr.length - 1)) / arr.length;
+        System.out.println("Average number is " + average);
     }
-
-    public static int gcd(int a, int b)
-    {
-        if (a == 0)
-            return b;
-
-        return gcd(b % a, a);
+    public static double findAvg(int[] arr, int n){
+        if(n == 0){
+            return arr[0];
+        }
+        else {
+            return arr[n] + findAvg(arr, n - 1);
+        }
     }
 }

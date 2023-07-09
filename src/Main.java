233a75a173dae6909406f2fde5cbf2d2e7b5a2f4
Aikusya <butterflyy1211@gmail.com>
2023-07-09 19:32:35 +0600

    calculate the factorial of the input number

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,18 @@ public class Main {
 
 // User inputs the number
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-// Checking if the number is prime or not
-        if (number == 1 || number == 0) {
-            System.out.println(number + " is not a prime neither prime number.");
-        } else {
-            if (isPrime(number, number / 2)) {
-                System.out.println(number + " is a prime number.");
-            }
-            else {
-                System.out.println(number + " is not a prime number.");
-            }
-        }
+        System.out.println("Enter the number: ");
+        int num = scanner.nextInt();
+        int result = factorial(num);
+        System.out.println(result);
     }
 
-// isPrime method checks if the number is prime or not using recursion
-    private static boolean isPrime(int number, int divisor) {
-        if (divisor == 1) {
-            return true;
+// factorial methods calculates the factorial of the input number
+    public static int factorial(int n){
+        if(n == 1){
+            return 1;
         }
-        if (number % divisor == 0) {
-            return false;
-        }
-        return isPrime(number, divisor - 1);
+        return n * factorial(n - 1);
     }
 }
 

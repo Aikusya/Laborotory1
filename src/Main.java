d6d82d92608268348c9e88cc3e08eba45d4f865c
Aikusya <butterflyy1211@gmail.com>
2023-07-09 19:58:49 +0600

    calculates the Great Common Divisor of two numbers

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,19 @@ public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
 // Input two numbers
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-// Calculate their binomial coefficient
-        int result = binomCoeff(n, k);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+// Find their GCD
+        int result = gcd(a, b);
         System.out.println(result);
     }
 
-// binomCoeff method calculates their binomial coefficient
-    static int binomCoeff(int n, int k) {
-        if(k == 0){
-            return 1;
-        }
-        if(k == n){
-            return 1;
-        }
-        return binomCoeff(n - 1, k - 1) + binomCoeff(n - 1, k);
+// gcd method calculates the Great Common Divisor of two numbers
+    public static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+
+        return gcd(b % a, a);
     }
 }

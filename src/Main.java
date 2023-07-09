226fe3b08cb2fb3528936fdb2fcab39d09d75c05
Aikusya <butterflyy1211@gmail.com>
2023-07-09 19:45:14 +0600

    reverse the order of members in array

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-// Input the number user wants to power
-        System.out.println("Enter the number u want to power: ");
-        int num = scanner.nextInt();
-// Input the number user wants to power the previous number
-        System.out.println("Enter the number u want to powered bby: ");
-        int power = scanner.nextInt();
-// Calculate the result
-        int result = power(num, power);
-        System.out.println(result);
+// Input array's size
+        System.out.println("Enter array size:");
+        int size = scanner.nextInt();
+// Set the members of array
+        int[] arr = setArray(size);
+// Reverse the array
+        reverseArray(arr, 0, arr.length - 1);
+// Print the reversed array
+        printArray(arr, arr.length);
     }
 
-// power method calculates the power of numbers
-    public static int power(int a, int n){
-        if(n == 0){
-            return 1;
+// setArray method used to set the members of array
+    public static int[] setArray(int size){
+        int[] arr = new int[size];
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < size; i ++){
+            System.out.println("Enter array's member");
+            int member = scanner.nextInt();
+            arr[i] = member;
         }
-        if(n == 1){
-            return a;
+        return arr;
+    }
+
+//reverseArray method used to reverse the order of members in array
+    static void reverseArray(int[] arr, int start, int end) {
+        int temp;
+        if (start >= end)
+            return;
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArray(arr, start+1, end-1);
+    }
+
+// printArray method used to print out all the members of array
+    static void printArray(int[] arr, int size) {
+        for (int i = 0; i < size; i++){
+            System.out.print(arr[i] + " ");
         }
-        return a * power(a, n - 1);
     }
 }
-

e73b643bb064942b455d8ead28b69fb0facbc117
Aikusya <butterflyy1211@gmail.com>
2023-07-09 19:52:15 +0600

    check if the input string consists of digits or not

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
+// Input any string
         Scanner scanner = new Scanner(System.in);
-// Input array's size
-        System.out.println("Enter array size:");
-        int size = scanner.nextInt();
-// Set the members of array
-        int[] arr = setArray(size);
-// Reverse the array
-        reverseArray(arr, 0, arr.length - 1);
-// Print the reversed array
-        printArray(arr, arr.length);
+        String string = scanner.next();
+        int n = string.length() - 1;
+// Check the string
+        check(string, n);
     }
 
-// setArray method used to set the members of array
-    public static int[] setArray(int size){
-        int[] arr = new int[size];
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < size; i ++){
-            System.out.println("Enter array's member");
-            int member = scanner.nextInt();
-            arr[i] = member;
+// check method is used to check if the input string consists of digits or not
+    static void check(String string, int n) {
+        if(n == 0){
+            if(Character.isDigit(string.charAt(n))){
+                System.out.println("YES");
+                return;
+            }
+            else{
+                System.out.println("NO");
+                return;
+            }
         }
-        return arr;
-    }
-
-//reverseArray method used to reverse the order of members in array
-    static void reverseArray(int[] arr, int start, int end) {
-        int temp;
-        if (start >= end)
+        if(Character.isDigit(string.charAt(n))){
+            check(string, n - 1);
+        }
+        else{
+            System.out.println("NO");
             return;
-        temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        reverseArray(arr, start+1, end-1);
-    }
-
-// printArray method used to print out all the members of array
-    static void printArray(int[] arr, int size) {
-        for (int i = 0; i < size; i++){
-            System.out.print(arr[i] + " ");
         }
     }
 }

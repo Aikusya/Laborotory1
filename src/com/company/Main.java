639c38b52632afde67627a2fd00e0251de82c827
Aikusya <butterflyy1211@gmail.com>
2023-07-14 13:15:58 +0600

    noth

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,40 +1,33 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-    // User inputs a size of an array
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter array size:");
-        int size = scanner.nextInt();
-    //Does set the size of array
-        int[] arr = setArray(size);
-    //Does find the min member of array
-        int min = findMin(arr, size);
-        System.out.println("Min number is " + min);
-    }
-
-    // setArray method is used to initialize the members of an array
-    public static int[] setArray(int size){
-        int[] arr = new int[size];
-        Scanner scanner = new Scanner(System.in);
-    // Loop is used to initialize members of array
-        for(int i = 0; i < size; i ++){
-            System.out.println("Enter array's member");
-            int member = scanner.nextInt();
-            arr[i] = member;
-        }
-        return arr;
-    }
-
-    // findMin method is used to find the min member of array by recursion
-    public static int findMin(int[] arr, int size){
-        if(size == 1){
-            return arr[0];
-        }
-        else{
-            int min = findMin(arr, size - 1);
-            return Math.min(min, arr[size-1]);
-        }
+        MyArrayList myArrayList = new MyArrayList();
+        myArrayList.add(4);
+        myArrayList.add(6);
+        myArrayList.add(8);
+        myArrayList.add(11);
+        myArrayList.add(15);
+        myArrayList.add(8);
+        myArrayList.add("h");
+        myArrayList.add(true);
+        myArrayList.add(false);
+        myArrayList.add(12.00);
+        //myArrayList.add(scanner.nextInt());
+        //System.out.println(myArrayList.get(0));
+        //System.out.println(myArrayList.size());
+        //System.out.println(myArrayList.getElement(15));
+        //myArrayList.clear();
+        //System.out.println(myArrayList.getSize());
+        //System.out.println(myArrayList.get(3));
+        //myArrayList.delete(3);
+        //System.out.println(myArrayList.get(3));
+        myArrayList.sort();
+        myArrayList.printArray();
     }
 }
